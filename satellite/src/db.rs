a94006daffde23c5b1424e4a0ff5e4db52d544db
Natalie B. <22222885+espeon@users.noreply.github.com>
2025-02-27 11:55:51 -0600

    censor db url

--- satellite/src/db.rs
+++ satellite/src/db.rs
@@ -2,11 +2,26 @@ use sqlx::postgres::{PgPool, PgPoolOptions};
 use std::env;
 use tracing::info;
 
+fn censor_url(url: &str) -> String {
+    let password_start = url.find(':').unwrap_or(0) + 1;
+    let password_end = url.find('@').unwrap_or(url.len());
+
+    if password_start > 0 && password_start < password_end && password_end <= url.len() {
+        let mut censored_url = String::with_capacity(url.len());
+        censored_url.push_str(&url[..password_start]);
+        censored_url.push_str("*****");
+        censored_url.push_str(&url[password_end..]);
+        censored_url
+    } else {
+        url.to_string()
+    }
+}
+
 pub async fn init_pool() -> anyhow::Result<PgPool, anyhow::Error> {
     info!(
             target: "db",
         "Connecting to the database at url {}",
-        &env::var("DATABASE_URL")?
+        censor_url(&env::var("DATABASE_URL")?)
     );
     let pool = PgPoolOptions::new()
         .max_connections(50)
