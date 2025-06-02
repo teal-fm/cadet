c54ab5019986436ed354edd330287892df5317a3
Natalie B. <22222885+espeon@users.noreply.github.com>
2025-06-02 14:26:12 -0500

    add getProfiles

--- aqua/src/xrpc/actor.rs
+++ aqua/src/xrpc/actor.rs
@@ -1,15 +1,22 @@
 use crate::ctx::Context;
-use axum::{Extension, http::StatusCode, response::IntoResponse};
+use axum::{Extension, http::StatusCode, response::IntoResponse, routing::get};
 use serde::{Deserialize, Serialize};
 use types::fm::teal::alpha::actor::defs::ProfileViewData;
+
+// mount actor routes
+pub fn actor_routes(app: axum::Router) -> axum::Router {
+    app.route("/fm.teal.alpha.actor.getProfile", get(get_actor))
+        .route("/fm.teal.alpha.actor.getProfiles", get(get_actors))
+}
+
+#[derive(Deserialize)]
+pub struct GetProfileQuery {
+    pub actor: Option<String>,
+}
 
 #[derive(Serialize)]
 pub struct GetProfileResponse {
     profile: ProfileViewData,
-}
-#[derive(Deserialize)]
-pub struct GetProfileQuery {
-    pub identity: Option<String>,
 }
 
 pub async fn get_actor(
@@ -17,7 +24,7 @@ pub async fn get_actor(
     axum::extract::Query(query): axum::extract::Query<GetProfileQuery>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
     let repo = &ctx.db; // assuming ctx.db is Box<dyn ActorProfileRepo + Send + Sync>
-    let identity = &query.identity;
+    let identity = &query.actor;
 
     if identity.is_none() {
         return Err((StatusCode::BAD_REQUEST, "identity is required".to_string()));
@@ -29,6 +36,33 @@ pub async fn get_actor(
     {
         Ok(Some(profile)) => Ok(axum::Json(GetProfileResponse { profile })),
         Ok(None) => Err((StatusCode::NOT_FOUND, "Profile not found".to_string())),
+        Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string())),
+    }
+}
+
+#[derive(Deserialize)]
+pub struct GetProfilesQuery {
+    pub actors: Vec<String>,
+}
+
+#[derive(Serialize)]
+pub struct GetProfilesResponse {
+    profiles: Vec<ProfileViewData>,
+}
+
+pub async fn get_actors(
+    Extension(ctx): Extension<Context>,
+    axum::extract::Query(query): axum::extract::Query<GetProfilesQuery>,
+) -> Result<impl IntoResponse, (StatusCode, String)> {
+    let repo = &ctx.db; // assuming ctx.db is Box<dyn ActorProfileRepo + Send + Sync>
+    let actor = &query.actors;
+
+    if actor.len() == 0 {
+        return Err((StatusCode::BAD_REQUEST, "actor is required".to_string()));
+    }
+
+    match repo.get_multiple_actor_profiles(actor).await {
+        Ok(profiles) => Ok(axum::Json(GetProfilesResponse { profiles })),
         Err(e) => Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string())),
     }
 }
